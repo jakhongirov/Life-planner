28d22756a8fbb86ce7e57690c4fb5d7583bcd3af
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-12-27 01:35:10 +0800

    add

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 const localText = require('./text.json')
 const model = require('./model')
 
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
+const { text } = require('stream/consumers');
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
    polling: {
       interval: 1000,
@@ -16,22 +17,152 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, {
    }
 });
 
+const productivity = [
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/prductivity1.png')
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/prductivity2.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/prductivity3.png'),
+   }
+]
+
+const tasks = [
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/task1.png')
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/task2.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/task3.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/task4.png'),
+   }
+]
+
+const habit = [
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/habit1.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/habit2.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/habit3.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/habit4.png'),
+   }
+]
+
+const all = [
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/ALL1.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/ALL2.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/ALL3.png'),
+   },
+   {
+      type: 'photo',
+      media: fs.readFileSync('./images/ALL4.png'),
+   }
+]
+
 bot.onText(/\/start/, async (msg) => {
    const chatId = msg.chat.id;
    const foundUser = await model.foundUser(chatId)
 
    if (foundUser) {
-
-      bot.sendMessage(chatId, localText?.freeFileLink).then(async () => {
-         bot.sendMessage(chatId, localText?.premiumFileText, {
-            reply_markup: {
-               inline_keyboard: [
-                  [{
-                     text: localText.clickBtn,
-                     url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=99000&additional_param4=Planner`
-                  }]
-               ],
-            }
+      bot.sendMessage(chatId, localText.communityText, {
+         reply_markup: {
+            keyboard: [
+               [
+                  {
+                     text: localText.communityBtn
+                  }
+               ]
+            ]
+         }
+      }).then(async () => {
+         bot.sendMediaGroup(chatId, productivity,).then(async () => {
+            bot.sendMessage(chatId, localText?.productivityText, {
+               reply_markup: {
+                  inline_keyboard: [
+                     [
+                        {
+                           text: localText?.clickBtnProductivity,
+                           url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=49000&additional_param4=Produktivlik`
+                        }
+                     ]
+                  ]
+               }
+            }).then(async () => {
+               bot.sendMediaGroup(chatId, tasks,).then(async () => {
+                  bot.sendMessage(chatId, localText?.taskText, {
+                     reply_markup: {
+                        inline_keyboard: [
+                           [
+                              {
+                                 text: localText?.clickBtnTask,
+                                 url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=49000&additional_param4=Vazifalar`
+                              }
+                           ]
+                        ]
+                     }
+                  })
+               }).then(async () => {
+                  bot.sendMediaGroup(chatId, habit,).then(async () => {
+                     bot.sendMessage(chatId, localText?.habitText, {
+                        reply_markup: {
+                           inline_keyboard: [
+                              [
+                                 {
+                                    text: localText?.clickBtnHabit,
+                                    url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=49000&additional_param4=Odatlar`
+                                 }
+                              ]
+                           ]
+                        }
+                     }).then(async () => {
+                        bot.sendMediaGroup(chatId, all).then(async () => {
+                           bot.sendMessage(chatId, localText?.allText, {
+                              reply_markup: {
+                                 inline_keyboard: [
+                                    [
+                                       {
+                                          text: localText?.clickBtnAll,
+                                          url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=99000&additional_param4=Barchasi`
+                                       }
+                                    ]
+                                 ]
+                              }
+                           })
+                        })
+                     })
+                  })
+               })
+            })
          })
       })
    } else {
@@ -82,20 +213,87 @@ bot.on('contact', async (msg) => {
       const addPhoneUser = await model.addUser(chatId, phoneNumber, name)
 
       if (addPhoneUser) {
-
-         bot.sendMessage(chatId, localText.freeFileLink).then(async () => {
-            bot.sendMessage(chatId, localText?.premiumFileText, {
-               reply_markup: {
-                  inline_keyboard: [
-                     [{
-                        text: localText.clickBtn,
-                        url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=99000&additional_param4=Planner`
-                     }]
-                  ],
-               }
+         bot.sendMessage(chatId, localText.communityText, {
+            reply_markup: {
+               keyboard: [
+                  [
+                     {
+                        text: localText.communityBtn
+                     }
+                  ]
+               ]
+            }
+         }).then(async () => {
+            bot.sendMediaGroup(chatId, productivity,).then(async () => {
+               bot.sendMessage(chatId, localText?.productivityText, {
+                  reply_markup: {
+                     inline_keyboard: [
+                        [
+                           {
+                              text: localText?.clickBtnProductivity,
+                              url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=49000&additional_param4=Produktivlik`
+                           }
+                        ]
+                     ]
+                  }
+               }).then(async () => {
+                  bot.sendMediaGroup(chatId, tasks,).then(async () => {
+                     bot.sendMessage(chatId, localText?.taskText, {
+                        reply_markup: {
+                           inline_keyboard: [
+                              [
+                                 {
+                                    text: localText?.clickBtnTask,
+                                    url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=49000&additional_param4=Vazifalar`
+                                 }
+                              ]
+                           ]
+                        }
+                     })
+                  }).then(async () => {
+                     bot.sendMediaGroup(chatId, habit,).then(async () => {
+                        bot.sendMessage(chatId, localText?.habitText, {
+                           reply_markup: {
+                              inline_keyboard: [
+                                 [
+                                    {
+                                       text: localText?.clickBtnHabit,
+                                       url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=49000&additional_param4=Odatlar`
+                                    }
+                                 ]
+                              ]
+                           }
+                        }).then(async () => {
+                           bot.sendMediaGroup(chatId, all).then(async () => {
+                              bot.sendMessage(chatId, localText?.allText, {
+                                 reply_markup: {
+                                    inline_keyboard: [
+                                       [
+                                          {
+                                             text: localText?.clickBtnAll,
+                                             url: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Lifeplanneruz&additional_param3=${chatId}&amount=99000&additional_param4=Barchasi`
+                                          }
+                                       ]
+                                    ]
+                                 }
+                              })
+                           })
+                        })
+                     })
+                  })
+               })
             })
          })
       }
+   }
+})
+
+bot.on('message', async (msg) => {
+   const chatId = msg.chat.id;
+   const text = msg.text;
+
+   if (text == localText.communityBtn) {
+      bot.sendMessage(chatId, localText.communityText)
    }
 })
 
@@ -116,4 +314,4 @@ app.get('/:chat_id', async (req, res) => {
    })
 })
 
-app.listen(5000, console.log(5000))
+app.listen(9000, console.log(9000))
